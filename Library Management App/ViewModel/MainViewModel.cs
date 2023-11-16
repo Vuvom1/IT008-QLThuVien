4f046300cc75e68b1673afdd8658afff498ff838
Vuvom1 <21522808@gm.uit.edu.vn>
2023-11-16 21:24:06 +0700

    Project Base Upload

---
+++ Library Management App/ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using Demo.ViewModel;
+using Library_Management_App.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Xml.Linq;
+
+namespace Library_Management_App.ViewModel
+{
+    internal class MainViewModel : BaseViewModel
+    {
+        public static Frame MainFrame { get; set; }
+
+        public void LoadTenND(MainView p)
+        {
+        }
+
+        void _Loadwd(MainView p)
+        {
+            
+        }
+        public void LoadQuyen(MainView p)
+        {
+            
+        }
+
+        public MainViewModel()
+        {
+           
+        }
+    }
+}
