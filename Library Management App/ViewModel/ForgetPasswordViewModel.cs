29152f23e5f838067cf733ae9a6b3df355c314d6
Vuvom1 <21522808@gm.uit.edu.vn>
2023-11-27 20:14:16 +0700

    Update UI Borrow/Add+Detail + UI Setting

---
+++ Library Management App/ViewModel/ForgetPasswordViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Library_Management_App.ViewModel;
+using Library_Management_App.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library_Management_App.ViewModel
+{
+
+    public class ForgetPasswordViewModel : BaseViewModel
+    {
+        public ICommand CancelCM { get; set; }
+        public ICommand SendPassCM { get; set; }
+        public ForgetPasswordViewModel()
+        {
+            CancelCM = new RelayCommand<object>((p) => { return p == null ? false : true; }, (p) =>
+            {
+                LoginViewModel.MainFrame.Content = new LoginPageView();
+            });
+        }
+    }
+}
---
+++ Library Management App/ViewModel/SignUpViewModel.cs
@@ -0,0 +1,15 @@
+﻿using Library_Management_App.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Library_Management_App.ViewModel
+{
+    internal class SignUpViewModel : BaseViewModel
+    {
+        public ICommand Closewd { get; set; }
+    }
+}
