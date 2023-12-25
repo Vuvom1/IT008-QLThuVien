b72d28ce026645c985063b369127a813620be24c
tuanh <tuanhdao2@gmail.com>
2023-12-25 20:40:37 +0700

    sửa lại file sql

--- Library Management App/Library Management App/ViewModel/BooksViewModel.cs
+++ Library Management App/Library Management App/ViewModel/BooksViewModel.cs
@@ -6,11 +6,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
+using System.Collections.ObjectModel;
+using Library_Management_App.Model;
 
 namespace Library_Management_App.ViewModel
 {
     internal class BooksViewModel: BaseViewModel
     {
+        private string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
+        private ObservableCollection<SACH> _ListBook;
+        public ObservableCollection<SACH> ListBook { get => _ListBook; set => _ListBook = value; }
         public ICommand AddPdPdCommand { get; set; }
 
         public BooksViewModel() 
