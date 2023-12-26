8a8c9e4e17f41264bf8ce99ffc4a2c1eab3753db
tuanh <tuanhdao2@gmail.com>
2023-12-26 09:04:07 +0700

    update thể loại

--- Library Management App/Library Management App/ViewModel/BooksViewModel.cs
+++ Library Management App/Library Management App/ViewModel/BooksViewModel.cs
@@ -16,10 +16,22 @@ namespace Library_Management_App.ViewModel
         private string _localLink = System.Reflection.Assembly.GetExecutingAssembly().Location.Remove(System.Reflection.Assembly.GetExecutingAssembly().Location.IndexOf(@"bin\Debug"));
         private ObservableCollection<SACH> _ListBook;
         public ObservableCollection<SACH> ListBook { get => _ListBook; set => _ListBook = value; }
-        public ICommand AddPdPdCommand { get; set; }
+        public ICommand AddBookCommand { get; set; }
+        public ICommand DetailBooksCommand { get; set; }
 
         public BooksViewModel() 
         {
+            AddBookCommand = new RelayCommand<BooksView>((p) => { return p == null ? false : true; }, (p) => _AddBookCommand(p));
+            DetailBooksCommand = new RelayCommand<BooksView>((p) => { return p.ListViewBooks.SelectedItem == null ? false : true; }, (p) => _DetailBooksCommand(p));
+        }
+
+        void _AddBookCommand(BooksView booksView)
+        {
+
+        }
+        void _DetailBooksCommand(BooksView booksView)
+        {
+
         }
     }
 }
